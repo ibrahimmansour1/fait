8e21b115e8614a4de7ab38c532ce5e20a29533a0
ibrahimmansour1 <ibrahimmansour58563@gmail.com>
2024-03-20 22:53:17 +0200

    onboarding_v1

--- lib/source/views/chat_bot/views/chat_bot_view_body_with_injures.dart
+++ lib/source/views/chat_bot/views/chat_bot_view_body_with_injures.dart
@@ -1,7 +1,9 @@
+import 'package:fait/source/routes.dart';
+import 'package:fait/source/theme/app_colors.freezed.dart';
 import 'package:fait/source/views/chat_bot/widgets/chat_bot_button_widget.dart';
 import 'package:flutter/material.dart';
 import '../../../../utils/app_export.dart';
-import '../../../widgets/custom_elevated_button.dart';
+import '../../../widgets/custom_future_animated_opacity_widget.dart';
 import '../widgets/chat_message_widget.dart';
 import '../widgets/custom_chat_bot_app_bar.dart';
 
@@ -41,7 +43,7 @@ class _ChatBotViewBodyWithInjuresState
     'Plantar fasciitis',
     'Rotator cuff tear',
   ];
-
+  bool isInjuriesSelected = false;
   void toggleInjury(String injury) {
     setState(() {
       if (selectedInjuries.contains(injury)) {
@@ -49,6 +51,7 @@ class _ChatBotViewBodyWithInjuresState
       } else {
         selectedInjuries.add(injury);
       }
+      isInjuriesSelected = true;
     });
   }
 
@@ -64,45 +67,64 @@ class _ChatBotViewBodyWithInjuresState
         height: mediaQueryData.size.height,
         decoration: const BoxDecoration(color: Color(0xFF282B4C)),
         child: SafeArea(
-          child: Container(
+          child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const CustomChatBotAppBar(),
-                const ChatMessageWidget(message: 'Awesome'),
-                const ChatMessageWidget(
-                  message: 'Select your injuries from the list below',
+                CustomFutureAnimatedOpacityWidget(
+                    waitingDurationInMilliSeconds: 1000,
+                    child: const ChatMessageWidget(message: 'Awesome')),
+                CustomFutureAnimatedOpacityWidget(
+                  waitingDurationInMilliSeconds: 2000,
+                  child: const ChatMessageWidget(
+                    message: 'Select your injuries from the list below',
+                  ),
                 ),
                 SizedBox(height: 10.v),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.4,
-                  child: ListView.builder(
-                    itemCount: availableInjuries.length,
-                    itemBuilder: (context, index) {
-                      final injury = availableInjuries[index];
-                      final isSelected = selectedInjuries.contains(injury);
-                      return ListTile(
-                          title: Text(
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: CustomFutureAnimatedOpacityWidget(
+                    waitingDurationInMilliSeconds: 3000,
+                    child: Wrap(
+                      spacing: 8,
+                      runSpacing: 8,
+                      children: availableInjuries.map((injury) {
+                        final isSelected = selectedInjuries.contains(injury);
+                        return ChoiceChip(
+                          label: Text(
                             injury,
                             style: isSelected
                                 ? CustomTextStyles.titleLargeInter
                                 : CustomTextStyles.titleLargeInter
                                     .copyWith(color: Colors.white),
                           ),
-                          trailing: isSelected
-                              ? const Icon(Icons.check_circle,
-                                  color: Colors.white)
-                              : null,
-                          onTap: () {
+                          selected: isSelected,
+                          onSelected: (selected) {
                             toggleInjury(injury);
-                          });
-                    },
+                          },
+                          selectedColor: const Color(0xFF8394CA),
+                          labelStyle: const TextStyle(color: Colors.white),
+                          backgroundColor: const Color(0xFF353767),
+                        );
+                      }).toList(),
+                    ),
                   ),
                 ),
                 SizedBox(height: 10.v),
-                const ChatMessageWidget(message: "Ok! Let's continue"),
-                const Spacer(),
-                const ChatBotButtonWidget(route: '/chat_bot_allergy'),
+                isInjuriesSelected
+                    ? CustomFutureAnimatedOpacityWidget(
+                        waitingDurationInMilliSeconds: 1000,
+                        child: const ChatMessageWidget(
+                            message: "Ok! Let's continue"))
+                    : const SizedBox(),
+                // const Spacer(),
+                isInjuriesSelected
+                    ? CustomFutureAnimatedOpacityWidget(
+                        waitingDurationInMilliSeconds: 2000,
+                        child: const ChatBotButtonWidget(
+                            route: AppRoutes.chatBotAllergy,))
+                    : const SizedBox(),
                 SizedBox(
                   height: 20.v,
                 ),
